b2493014fc81b5458b7b9dbb79dcadf08d2aaadf
krloSc <65464258+krloSc@users.noreply.github.com>
2020-05-16 14:48:52 -0400

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,13 @@
+from util.data import *
+####################   preprocessing Data   ###################################
+data=Data()
+data.import_data("heart")
+data.correlation("result")
+#data.corr_matrix(save=True)
+data.encoder("cp")
+train_set, test_set = data.split(0.2)
+train_labels=train_set["result"].copy()
+train_labels=test_set["result"].copy()
+train_prepared, test_prepared = data.prepare()
+
+###########################    Training    ####################################
