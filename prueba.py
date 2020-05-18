ca9b0c259aea98e5c8f43c73a2298e06e0334104
krloSc <krlos@krlos-laptop>
2020-05-17 21:11:48 -0400

    updated

--- prueba.py
+++ prueba.py
@@ -1,6 +1,8 @@
 from util.data import *
 from training.train import*
+
 ####################   preprocessing Data   ###################################
+
 data=Data()
 data.import_data("heart")
 data.correlation("result")
@@ -10,7 +12,9 @@ train_set, test_set = data.split(0.2)
 train_labels=train_set["result"].copy()
 test_labels=test_set["result"].copy()
 train_prepared, test_prepared = data.prepare()
+
 ###########################    Training    ####################################
+
 train=training()
 params=({'tipo':SVC(),'param':{'kernel': ["rbf"], 'coef0': [0, 1,10], 'gamma': [ 0.1, 1, 5], 'C':[ 0.01, 1, 5]}},
     {'tipo':SVC(),'param':{'kernel': ["poly"],'degree':[2,3], 'coef0': [3,5,10,50], 'C':[ 0.01, 1, 5,30]}},
@@ -19,3 +23,5 @@ params=({'tipo':SVC(),'param':{'kernel': ["rbf"], 'coef0': [0, 1,10], 'gamma': [
 training.search_best(training,*params,heart=train_prepared,labels=train_labels)
 for item in training.best:
         print(item)
+
+##############################   Evaluating   ###################################
