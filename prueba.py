7d87639def1a34a52a0c26aaa19f1fa08ce9e450
krloSc <krlos@krlos-laptop>
2020-05-18 23:26:56 -0400

    updated

--- prueba.py
+++ prueba.py
@@ -9,7 +9,7 @@ data.import_data("heart")
 data.correlation("result")
 #data.corr_matrix(save=True)
 data.encoder("cp")
-train_set, test_set = data.split(0.2)
+train_set, test_set = data.split(0.3)
 train_labels=train_set["result"].copy()
 test_labels=test_set["result"].copy()
 train_prepared, test_prepared = data.prepare()
@@ -21,12 +21,14 @@ params=({'tipo':SVC(),'param':{'kernel': ["rbf"], 'coef0': [0, 1,10], 'gamma': [
     {'tipo':SVC(),'param':{'kernel': ["poly"],'degree':[2,3], 'coef0': [3,5,10,50], 'C':[ 0.01, 1, 5,30]}},
     {'tipo':LinearSVC(),'param':{'C':[0.1,1,3,5],'max_iter':[10000]}},
     {'tipo':LinearSVC(),'param':{'max_iter':[100000],'C':[0.1,1,3,5]}})
-training.search_best(training,*params,heart=train_prepared,labels=train_labels)
+training.search_best(training,*params,heart=train_prepared,labels=train_labels,out=False)
 
 
-##############################   Evaluating   ###################################
-
+print('################################   Evaluating   #####################################')
+print('{0:70} Recall Precision'.format('Parameters'))
 for clf, title in zip(training.best_models,training.best_params):
     clf.fit(train_prepared,train_labels)
-    print(str(title),"\t",recall_score(clf.predict(test_prepared),test_labels),"\t",
-        precision_score(clf.predict(test_prepared),test_labels))
+
+    #print("{0:10}".format(str(type(clf))),"\t\t","%1.3f \t %1.3f" % (recall_score(clf.predict(test_prepared),test_labels),
+    #    precision_score(clf.predict(test_prepared),test_labels)))
+    print('{0:70}  %.3f %.3f'.format(str(title)) %(recall_score(clf.predict(test_prepared),test_labels),precision_score(clf.predict(test_prepared),test_labels)))
--- training/train.py
+++ training/train.py
@@ -9,12 +9,11 @@ class training():
 	def __init__(self):
 		pass
 
-	def search_best(self,*kwargs,heart,labels,folds=5,score='f1'):
+	def search_best(self,*kwargs,heart,labels,folds=5,score='f1',out=False):
 		for svm in kwargs:
-			print(svm)
 			grid_search = GridSearchCV(svm.get('tipo'),
 						svm.get('param'), cv=folds,
-			scoring=score,verbose=1)
+			scoring=score,verbose=out)
 			grid_search.fit(heart, labels)
 			self.best_params.append(grid_search.best_params_)
 			self.best_models.append(grid_search.best_estimator_)
